0ed6aa70dcacbb41cf9ede3418710d09fd8ba97c
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2025-06-07 03:27:05 +0800

    Init Project

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace FastCopy.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FastCopy.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
